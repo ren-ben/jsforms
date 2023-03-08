a506f7c4cc9f693aa8f775d8ebab20b36c030573
ben <OliGaming122@gmail.com>
2023-03-08 13:24:36 +0100

    add basic form generator
    add javascript for forms

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 document.getElementById("druckansicht").addEventListener("click", function () {
   let data = document.querySelectorAll("input");
-  let select = document.getElementById("select").value;
-  let checkbox = document.getElementById("checkbox").checked;
-  let date = document.getElementById("date").value;
-  console.log(data);
+  let select = document.getElementById("pizzenauswahl").value;
+  let checkbox = document.getElementById("extras").checked;
+  let date = document.getElementById("geburtsdatum").value;
+
+  let gender = data[4].checked ? "r Herr" : " Frau";
+
+  let text =
+    "Sehr geehrte" +
+    gender +
+    " " +
+    data[0].value +
+    " " +
+    data[1].value +
+    " " +
+    data[2].value +
+    " " +
+    data[3].value +
+    " " +
+    select +
+    " " +
+    checkbox +
+    " " +
+    date +
+    "";
+
+  document.getElementById("lelele").innerHTML = text;
 });
