f1381cd8a7dc036292d1b72275625228d064dc37
ben <OliGaming122@gmail.com>
2023-03-08 06:49:02 +0100

    add button detection
    add basic button detection

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("druckansicht").addEventListener("click", function () {
+  let data = document.querySelectorAll("input");
+  let select = document.getElementById("select").value;
+  let checkbox = document.getElementById("checkbox").checked;
+  let date = document.getElementById("date").value;
+  console.log(data);
+});
